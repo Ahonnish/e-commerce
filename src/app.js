f8f295a6822f46be8d920df625e4b622faa6c720
Ahonnish <ahonnish3@gmail.com>
2026-05-06 15:00:56 +0530

    Created signup API with validation and hashing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const healthRouter = require("./routes/health.routes");
+const authRoutes = require("./routes/auth.routes")
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // middleware
+
 app.use("/api", healthRouter);
+app.use("/api/auth",authRoutes)
 
 module.exports = app;
